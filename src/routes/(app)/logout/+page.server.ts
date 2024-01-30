057beb0aa413f40a34625c3a6678def797dda6da
Jolan <jolan.gorges@outlook.com>
2024-01-30 23:09:59 +0100

    feat(auth): add logout page

---
+++ src/routes/(app)/logout/+page.server.ts
@@ -0,0 +1,16 @@
+import { fail, redirect } from '@sveltejs/kit';
+import lucia from '$lib/server/auth';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ locals, cookies }) => {
+	if (!locals.session) {
+		return fail(401);
+	}
+	await lucia.invalidateSession(locals.session.id);
+	const sessionCookie = lucia.createBlankSessionCookie();
+	cookies.set(sessionCookie.name, sessionCookie.value, {
+		path: '.',
+		...sessionCookie.attributes
+	});
+	redirect(302, '/login');
+};
