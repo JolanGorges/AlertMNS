6173d6611a9f8f59098251ee3c3d25a49bb9216e
Jolan <jolan.gorges@outlook.com>
2024-01-31 09:43:15 +0100

    feat(ui): add admin route

---
+++ src/lib/components/navigation/NavigationAdmin.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import ifa from '$lib/assets/ifa.png';
+	import mns from '$lib/assets/mns.png';
+</script>
+
+<div class="p-4 h-full flex flex-col">
+	<a href="/" class="flex justify-evenly items-center py-2">
+		<img src={ifa} alt="IFA" class="w-8" />
+		<img src={mns} alt="MNS" class="w-16" />
+	</a>
+	<nav class="list-nav" aria-labelledby="admin">
+		<h2 id="admin" class="mx-4 my-2">Paramètres administrateur</h2>
+		<ul>
+			<li><a href="/admin/users">Gérer les utilisateurs</a></li>
+			<li><a href="/admin/users/new">Créer un utilisateur</a></li>
+			<li><a href="/admin/groups">Gérer les groupes</a></li>
+			<li><a href="/admin/groups/new">Créer un groupe</a></li>
+		</ul>
+	</nav>
+</div>
---
+++ src/routes/admin/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../../app.postcss';
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	import NavigationAdmin from '$lib/components/navigation/NavigationAdmin.svelte';
+</script>
+
+<AppShell slotSidebarLeft="bg-surface-500/5 w-70">
+	<svelte:fragment slot="header"></svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<NavigationAdmin />
+	</svelte:fragment>
+	<slot />
+</AppShell>
